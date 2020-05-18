2e04d6cafb32811100f2594be6aadd4a143c10c0
Samantha Andre <samantha.s.andre@gmail.com>
2020-05-18 17:43:19 -0400

    added controllers and views

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,19 @@
+class RequestsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def incoming
+  end
+
+  def outgoing
+  end
+end
