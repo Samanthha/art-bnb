4fbc00043421107aed44fb990e00d4eb61907fe7
Daniela Santana <daanims@gmail.com>
2020-05-19 11:27:17 -0400

    added reviews controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,23 @@
 class ReviewsController < ApplicationController
   def new
+    @paiting = Painting.find(params[:painting_id]) 
+    @review = Review.new
   end
 
   def create
+    @painting = Painting.find(params[:painting_id])
+    @review = Review.new(review_params)
+    @review.painting = @painting
+    if @review.save
+      redirect_to painting_path(@painting)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
