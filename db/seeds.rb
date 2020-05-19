8e16b1e2a0616e5dacf57f86e1fd9d8861fbee0b
Solene Duprat <solene.duprat@gmail.com>
2020-05-19 14:52:25 -0400

    Seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.new(email: "solene@email.com", password: "123456").save
+User.new(email: "daniela@email.com", password: "123456").save
+User.new(email: "celine@email.com", password: "123456").save
+User.new(email: "samantha@email.com", password: "123456").save
+
+User.all.each do |user|
+  rand(2..5).times do
+    Painting.new(name: Faker::Artist.name,
+                 painter: Faker::Hipster.word.capitalize,
+                 category: %w[Impressionism Realism Renaissance Street-Art Abstract Modern].sample,
+                 location: Faker::Address.city,
+                 dimensions: "#{rand(30..400)} x #{rand(30..400)}",
+                 price_per_day: rand(20..500),
+                 user: user
+                 ).save
+  end
+end
+
+Painting.all.each do |painting|
+  rand(2..5).times do
+    rating = rand(0..5)
+    content = ["good quality", "well delivered", "I hosted a great dinner party", "perfect for realtors like me who need fancy paintings", "OK service", "Person was very punctual and helpful", "good", "Ok thanks", "nice!"].sample
+    Review.create(
+      rating: rating,
+      content: content,
+      painting: painting,
+      user: User.all.sample
+      )
+  end
+end
