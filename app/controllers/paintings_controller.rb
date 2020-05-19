611b4a79f75eab0c913d00e1a8914a6d7761cd6b
Samantha Andre <samantha.s.andre@gmail.com>
2020-05-19 11:24:27 -0400

    completed paintings#controller

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,25 +1,45 @@
 class PaintingsController < ApplicationController
   def index
+    @paintings = Painting.all
   end
 
   def show
+    @painting = Painting.find(params[:id])
   end
 
   def new
+    @painting = Painting.new
   end
 
   def create
+    new_painting = Painting.new(painting_params)
+    new_painting.save
+    redirect_to painting_path(new_painting)
   end
 
   def edit
+    @painting = Painting.find(params[:id])
   end
 
   def update
+    @painting = Painting.find(params[:id])
+    @painting.update(painting_params)
+
+    redirect_to painting_path(@painting)
   end
 
   def destroy
+    @painting = Painting.find(params[:id])
+    @painting.destroy
   end
 
   def delete
+
+  end
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:name, :painter, :category, :location, :dimensions, :price_per_day)
   end
 end
