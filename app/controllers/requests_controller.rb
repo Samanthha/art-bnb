dad53a7b7046d5836cce8d20eaf7b79d5415d3d3
Daniela Santana <daanims@gmail.com>
2020-05-20 11:37:00 -0400

    started requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,19 +1,38 @@
 class RequestsController < ApplicationController
   def new
+    @painting = Painting.find(params[:painting_id])
+    @request = Request.new
   end
 
   def create
+    @painting = Painting.find(params[:painting_id])
+    @request = Request.new
+    @request.painting = @painting
+    @request.user = current_user
+    if @request.save
+      redirect_to request_path(@request)
+    else
+      render :new
+    end
   end
 
   def show
+    @request = Request.find(params[:id])
   end
 
   def update
+    @request = Request.find(params[:id])
   end
 
   def incoming
   end
 
   def outgoing
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:start_date, :end_date)
   end
 end
