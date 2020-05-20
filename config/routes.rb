ba9f5414c5b35220208a53d5b8a78ddc7d0b98b2
Solene Duprat <solene.duprat@gmail.com>
2020-05-20 12:38:18 -0400

    Added my profile and links to navbar now working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'paintings#index'
+  root to: 'pages#home'
   resources :paintings do
     resources :reviews, only: [:new, :create]
     resources :requests, only: [:new, :create]
