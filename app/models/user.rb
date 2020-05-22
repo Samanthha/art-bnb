0751f803554b4b50d79aeb5f6f146dd63ee343be
CelineRondeau <rondeau9314@gmail.com>
2020-05-22 11:47:39 -0400

    Adding validations to review & user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :paintings, dependent: :destroy
   has_many :reviews
   has_many :requests, dependent: :destroy
+  validates :first_name, length: { minimum: 3 }
+  validates :last_name, length: { minimum: 3 }
 
   def name
     "#{first_name} #{last_name}"
