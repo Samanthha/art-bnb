05c02de36795813b53154fc4fc12fb73d83c73e7
Solene Duprat <solene.duprat@gmail.com>
2020-05-22 14:57:34 -0400

    small change to validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ApplicationRecord
   belongs_to :painting
   belongs_to :user
   validates :content, presence: true
-  validates :content, length: { minimum: 10 }
+  validates :content, length: { minimum: 2 }
   validates :rating, presence: true
   validates :rating, numericality: { in: 0..5 }
-  validates :content, length: { minimum: 10 }
+  validates :content, length: { maximum: 100 }
 end
