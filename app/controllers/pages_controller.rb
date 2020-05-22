be0633614cea5cacbdb8806119c63838683d70b6
Daniela Santana <daanims@gmail.com>
2020-05-22 12:08:07 -0400

    changing profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,6 @@ class PagesController < ApplicationController
     @user = current_user
     @paintings = Painting.where(user: current_user)
     @current_requests = Request.where(painting: @user.paintings)
+    
   end
 end
